f5c3a0adae4e1abdd14eb6e3048ea180cbcf5ea8
Mustafa Andich <mustafa.andich@gmail.com>
2021-12-06 15:37:30 +0300

    session2

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,8 +1,55 @@
-import { Component } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
+import { Card } from '../app.component'
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {}
+export class CardComponent implements OnInit {
+
+  @Input() card!: Card
+  @Input() index!: number
+
+  title = 'My Card Title'
+  text = 'My Semple Text'
+
+  cardDate: Date = new Date()
+
+  textColor = "blue"
+
+  disabled = false
+
+  number = 42
+
+  array = [1, 1, 2, 3, 5, 8, 13]
+
+  obj = {name: 'Mustafa', info: {age: 21, weight: 77, height: 185}}
+
+  imgUrl = 'https://i1.wp.com/ilirhushi.me/wp-content/uploads/2018/03/angular_logo.png?fit=250%2C250&ssl=1'
+
+  ngOnInit() {
+    setTimeout( () => {
+      this.imgUrl = 'https://cdn.iconscout.com/icon/free/png-256/vue-dot-js-3629145-3030285.png'
+      this.disabled = true
+    }, 3000)
+  }
+
+  changeTitle() {
+    this.card.title = 'Другое название'
+  }
+
+  inputHandler(event: any) {
+    const value = event.target.value
+    this.card.title = value
+  }
+
+  changeHandler() {
+    console.log(this.card.title);
+    
+  }
+
+  getInfo() {
+    return 'This is my info'
+  }
+}
